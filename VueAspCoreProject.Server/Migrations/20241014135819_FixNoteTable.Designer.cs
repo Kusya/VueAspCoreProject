39ccc72790e2ec8a59ee766e630812bf8ea127de
Kusya <nika_nikusya@mail.ru>
2024-10-15 13:41:16 +0300

    Move connection string for database into appsettings.json. Fix Note model.

--- VueAspCoreProject.Server/Migrations/20241014135819_FixNoteTable.Designer.cs
+++ VueAspCoreProject.Server/Migrations/20241014135819_FixNoteTable.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using VueAspCoreProject.Server.Repository;
 
@@ -10,9 +11,11 @@ using VueAspCoreProject.Server.Repository;
 namespace VueAspCoreProject.Server.Migrations
 {
     [DbContext(typeof(NoteContext))]
-    partial class NoteContextModelSnapshot : ModelSnapshot
+    [Migration("20241014135819_FixNoteTable")]
+    partial class FixNoteTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ VueAspCoreProject.Server/Repository/NoteContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VueAspCoreProject.Server.Repository
+{
+    public class NoteContext : DbContext
+    {
+        //public NoteContext() : base(DbContextOptions)//("DefaultConnection")
+        //{ }
+        public DbSet<Note> Notes { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            string projectPath = AppDomain.CurrentDomain.BaseDirectory.Split(new String[] { @"bin\" }, StringSplitOptions.None)[0];
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(projectPath)
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connectionString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder
+                .UseSqlServer(
+                    connectionString,
+                    providerOptions => { providerOptions.EnableRetryOnFailure(); });
+        }
+
+    }
+
+    public class Note
+    {
+        public int NoteId { get; set; }
+        public string? NoteName { get; set; }
+        public string? NoteContent { get; set; }
+        public string? NoteColor { get; set; }
+
+    }
+}
